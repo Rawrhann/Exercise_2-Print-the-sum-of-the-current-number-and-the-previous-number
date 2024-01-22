df578809ea15d0ed2666d322c4f817ca31db0627
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-22 21:50:21 +0800

    Working Code

--- exercise_2.py
+++ exercise_2.py
@@ -2,7 +2,7 @@
 #Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
 
 number = 0
-for i in range(1, 10):
-    print(number)
-    number = number + i
-    
+for i in range(1, 11):
+    sum = number + i
+    print("The previous number is", number, "and the current number is", i, "and their sum is", sum)
+    number = i
